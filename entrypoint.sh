a22bc19b9ea673310d11adbbc3aa0d760e0aec62
Antoine Eiche <lewo@abesis.fr>
2019-09-25 12:15:45 +0200

    Do not build the image if it has been already pushed

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,8 @@ done
 
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker/config.json}
 REGISTRY=$(echo "$DESTINATION" | cut -d"/" -f1)
-SKOPEO_CMD=(skopeo copy)
+SKOPEO_COPY_CMD=(skopeo copy)
+SKOPEO_INSPECT_CMD=(skopeo inspect)
 
 if jq -e "(. | has(\"credHelpers\")) and (.credHelpers.\"$REGISTRY\" | . == \"ecr-login\")" < "$DOCKER_CONFIG" > /dev/null
 then
@@ -36,18 +37,27 @@ then
     AWS_REGION=$(echo "$AWS_REGISTRY_URL" | awk -F "." '{print $(NF-2)}')
     echo "* get credentials for the AWS registry on AWS region" "$AWS_REGION"
     PWD=$(aws ecr get-login --no-include-email --region "$AWS_REGION" | cut -d" " -f6)
-    SKOPEO_CMD+=(--dest-creds AWS:"${PWD}")
+    SKOPEO_COPY_CMD+=(--dest-creds AWS:"${PWD}")
+    SKOPEO_INSPECT_CMD+=(--creds AWS:"${PWD}")
 fi
 
 echo "* cd to ${CONTEXT}...."
 cd "${CONTEXT}"
 
-echo "* build started...."
-nix-build
-HASH=$(realpath ./result | xargs basename | cut -d- -f1)
+echo "* check if the image has been already pushed "
+HASH=$(nix-instantiate | xargs nix-store -q | cut -d"/" -f 4 | cut -d"-" -f 1)
+if "${SKOPEO_INSPECT_CMD[@]}" --tls-verify=false docker://"${DESTINATION}":"${HASH}" > /dev/null
+then
+    echo "* skip the build since the image has been already pushed...."
+else
+    echo "* build started...."
+    # TODO: do not create ./result
+    nix-build
+    HASH=$(realpath ./result | xargs basename | cut -d- -f1)
 
-echo "* push to registry: ${DESTINATION}:${HASH}"
-"${SKOPEO_CMD[@]}" --dest-tls-verify=false docker-archive://"${PWD}"/result docker://"${DESTINATION}":"${HASH}"
+    echo "* push to registry: ${DESTINATION}:${HASH}"
+    "${SKOPEO_COPY_CMD[@]}" --dest-tls-verify=false docker-archive://"${PWD}"/result docker://"${DESTINATION}":"${HASH}"
+fi
 
 if [[ -n ${IMAGE_MANIFEST_FILEPATH+x} ]];
 then
