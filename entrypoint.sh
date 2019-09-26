c2f0d75228f7bc3740b7c4e9452ba27212ced6d0
Antoine Eiche <lewo@abesis.fr>
2019-09-26 16:56:37 +0200

    Fix index.json file generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,9 +44,12 @@ fi
 echo "* cd to ${CONTEXT}...."
 cd "${CONTEXT}"
 
-echo "* check if the image has been already pushed "
+echo "* generating the image output path hash"
 HASH=$(nix-instantiate | xargs nix-store -q | cut -d"/" -f 4 | cut -d"-" -f 1)
-if "${SKOPEO_INSPECT_CMD[@]}" --tls-verify=false docker://"${DESTINATION}":"${HASH}" > /dev/null
+IMAGE_REF="$DESTINATION":"$HASH"
+
+echo "* check if the image $IMAGE_REF has been already pushed"
+if "${SKOPEO_INSPECT_CMD[@]}" --tls-verify=false docker://"$IMAGE_REF" > /dev/null
 then
     echo "* skip the build since the image has been already pushed...."
 else
@@ -55,13 +58,15 @@ else
     nix-build
     HASH=$(realpath ./result | xargs basename | cut -d- -f1)
 
-    echo "* push to registry: ${DESTINATION}:${HASH}"
-    "${SKOPEO_COPY_CMD[@]}" --dest-tls-verify=false docker-archive://"${PWD}"/result docker://"${DESTINATION}":"${HASH}"
+    echo "* push to registry: ${IMAGE_REF}"
+    "${SKOPEO_COPY_CMD[@]}" --dest-tls-verify=false docker-archive://"${PWD}"/result docker://"$IMAGE_REF"
 fi
 
 if [[ -n ${IMAGE_MANIFEST_FILEPATH+x} ]];
 then
-    echo "* exposing the image manifest"
+    echo "* exposing the image manifest to ${IMAGE_MANIFEST_FILEPATH}"
     mkdir -p "$(dirname "${IMAGE_MANIFEST_FILEPATH}")"
-    skopeo inspect docker://"${DESTINATION}":"${HASH}" > "${IMAGE_MANIFEST_FILEPATH}"
+    # This is to expose the digest id for Tekton. It takes a OCI index.json file. See
+    # https://github.com/tektoncd/pipeline/blob/3c46dcd0fbb524b0aa96bbed77977ccd26d6687f/cmd/imagedigestexporter/main.go#L34
+    skopeo inspect docker://"$IMAGE_REF" | jq '{ "manifests": [ . ] }' | tee "${IMAGE_MANIFEST_FILEPATH}"
 fi
